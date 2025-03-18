a3b22450e396a348c34ec89cf99d547c704a84c5
Indriyani2022 <Indriyani20222@gmail.com>
2025-03-19 06:50:28 +0700

    Update opening web.js

--- opening web.js
+++ opening web.js
@@ -1,7 +1,10 @@
+// Definisi fungsi
 function gotolib() {
     const userConfirmation = confirm("Apakah Anda yakin ingin membuka halaman perpustakaan?");
-
     if(userConfirmation) {
-        window.open('https://library.usu.ac.id/', '_blank');   
+        window.open('https://library.usu.ac.id/', '_blank');
     }
 }
+
+// Hubungkan fungsi ke sebuah tombol atau link
+document.getElementById('library-button').addEventListener('click', gotolib);
