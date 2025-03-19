77603a8b1109c43c2652d2b6a212de5f51e0926a
Indriyani2022 <Indriyani20222@gmail.com>
2025-03-19 07:02:44 +0700

    Update opening web.js

--- opening web.js
+++ opening web.js
@@ -1,9 +1,6 @@
 function gotolib() {
     const userConfirmation = confirm("Apakah Anda yakin ingin membuka halaman perpustakaan?");
     if(userConfirmation) {
-        const newWindow = window.open('https://library.usu.ac.id/', '_blank');
-        if (!newWindow || newWindow.closed || typeof newWindow.closed == 'undefined') {
-            alert("Popup diblokir oleh browser. Silakan izinkan popup untuk website ini.");
-        }
+        window.location.href = 'https://library.usu.ac.id/';
     }
 }
